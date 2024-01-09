1a8ac7b98245f5076bfa013a2e71d0aa28111e14
importantsgit <dlwognsdk@tukorea.ac.kr>
2024-01-09 12:50:27 +0900

    [이재훈] settingView 추가

---
+++ VoiceMemo/VoiceMemo/Feature/Home/Setting/SettingView.swift
@@ -0,0 +1,167 @@
+//
+//  SettingView.swift
+//  VoiceMemo
+//
+//  Created by 이재훈 on 2024/01/09.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        
+        VStack {
+            TitleView()
+            
+            Spacer()
+                .frame(height: 35)
+            
+            TotalTapCountView()
+            
+            Spacer()
+                .frame(height: 40)
+            
+            TotalTapMovieView()
+            
+            Spacer()
+        }
+    }
+}
+
+// MARK: - 타이틀 뷰
+private struct TitleView: View {
+    fileprivate var body: some View {
+        HStack {
+            Text("설정")
+                .font(.system(size: 30, weight: .bold))
+                .foregroundColor(.customBlack)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+        .padding(.top, 45)
+    }
+}
+
+// MARK: - 탭 카운트 뷰
+private struct TotalTapCountView: View {
+    fileprivate var body: some View {
+        // 각각 탭 카운트 뷰 (todolist, 메모장, 음성메모)
+        HStack {
+            
+            TapCountView(title: "ToDo", count: 0)
+            
+            Spacer()
+                .frame(width: 70)
+            
+            TapCountView(title: "Memo", count: 0)
+            
+            Spacer()
+                .frame(width: 70)
+            
+            TapCountView(title: "VoiceMemo", count: 0)
+            
+        }
+    }
+}
+
+// MARK: - 각 탭 설정된 카운트 뷰 (공통 뷰 컴포넌트)
+private struct TapCountView: View {
+    private var title: String
+    private var count: Int
+    
+    fileprivate init(
+        title: String,
+        count: Int
+    ) {
+        self.title = title
+        self.count = count
+    }
+    
+    fileprivate var body: some View {
+        VStack(spacing: 5) {
+            Text(title)
+                .font(.system(size: 14))
+                .foregroundColor(.customBlack)
+            
+            Text("\(count)")
+                .font(.system(size: 30, weight: .medium))
+                .foregroundColor(.customBlack)
+        }
+    }
+}
+
+// MARK: - 전체 탭 이동 뷰
+private struct TotalTapMovieView: View {
+    
+    fileprivate var body: some View {
+        VStack {
+            Rectangle()
+                .fill(Color.customGray1)
+                .frame(height: 1)
+            
+            TapMoveView(
+                title: "To Do List",
+                tapAction: {}
+            )
+            
+            TapMoveView(
+                title: "메모장",
+                tapAction: {}
+            )
+            
+            TapMoveView(
+                title: "음성메모",
+                tapAction: {}
+            )
+            
+            TapMoveView(
+                title: "타이머",
+                tapAction: {}
+            )
+            
+            Rectangle()
+                .fill(Color.customGray1)
+                .frame(height: 1)
+        }
+    }
+}
+
+// MARK: - 각 탭 이동 뷰
+private struct TapMoveView: View {
+    private var title: String
+    private var tapAction: () -> Void
+    
+    fileprivate init(
+        title: String,
+        tapAction: @escaping () -> Void
+    ) {
+        self.title = title
+        self.tapAction = tapAction
+    }
+    
+    fileprivate var body: some View {
+        Button(
+            action: tapAction,
+            label: {
+                HStack {
+                    Text(title)
+                        .font(.system(size: 14))
+                        .foregroundColor(.customBlack)
+                    
+                    Spacer()
+                    
+                    Image("arrowRight")
+                }
+            }
+        )
+        .padding(20)
+
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
